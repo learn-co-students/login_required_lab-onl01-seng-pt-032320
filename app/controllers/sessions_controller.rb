5f0c1a05fea0c2030efcc49295203b61b14b15d6
Vlad <VladDaImpaler@gmail.com>
2020-09-04 16:58:45 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     end
     def create
         binding.pry
+        session[:name] = params[:name] unless (params[:name].nil? || params[:name].empty?)
         #:name => "Trinity"
         #sets session[:name] with params
         #redirects to login if empty or nil
